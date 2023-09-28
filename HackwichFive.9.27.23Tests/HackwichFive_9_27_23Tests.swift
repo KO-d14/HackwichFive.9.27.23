c52c314587985ddeba70e65cc02b3049a7ebf368
KO-d14 <ainajk@hawaii.edu>
2023-09-29 08:01:00 +0900

    Initial Commit

---
+++ HackwichFive.9.27.23Tests/HackwichFive_9_27_23Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HackwichFive_9_27_23Tests.swift
+//  HackwichFive.9.27.23Tests
+//
+//  Created by Aina Kodaira on 9/29/23.
+//
+
+import XCTest
+@testable import HackwichFive_9_27_23
+
+class HackwichFive_9_27_23Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
