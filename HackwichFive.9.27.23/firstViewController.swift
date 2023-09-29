2b54f77a2dd315abb554bdf5477bef25e1cb7881
KO-d14 <ainajk@hawaii.edu>
2023-09-29 09:24:43 +0900

    -added some more comments in firstViewController file - Completed problem set for tab 2

--- HackwichFive.9.27.23/firstViewController.swift
+++ HackwichFive.9.27.23/firstViewController.swift
@@ -12,7 +12,7 @@ import UIKit
 // NOTE: CREATED NAV BAR CONTROLLER EVEN THO INSTRUCTION DOESN'T SAY
 
 // PART 4: Set Top Label’s Text (2 points)
-    /*name as "My Favorite Foods:" */
+    /*1. Set the topLabel’s text to: “My Favorite Foods:” (in code) and have the text display in the label when the app first opens. Please note this is the top label on your VC.*/
 
 // PART 5: Declare and Assign two variables (2 points)
 //1. Declare a variable of type Integer and name it “currentIndex” and assign it to the  integer, 0.
@@ -21,17 +21,25 @@ import UIKit
 var favoriteFoodsArray = ["Ice Cream", "Dim Sum", "Bread", "Sushi", "Soup/Stew-Related"]
 
 // PART 7: Set an image for the Tab Bar Item and Change the attributes for the button (2 points)
-/*
- 1.Set an image for the Tab Bar Item.
- 2.Change attributes for the button (if you havenʻt yet)
-3. Change the text for the button to “See My Fave Foods”
-4. Run the simulator. Please feel free to review my simulator recording. Also, what happens if you press the button more than once? Notice that if you press the button again, it doesn’t iterate through the array. What code do we need to add to our IBAction that will enable the button to iterate through the array upon a button press? We will work on this part in Week 7.
-*/
+    /*
+     1.Set an image for the Tab Bar Item.
+     2.Change attributes for the button (if you havenʻt yet)
+     3. Change the text for the button to “See My Fave Foods”
+     4. Run the simulator. Please feel free to review my simulator recording. Also, what happens if you press the button more than once? Notice that if you press the button again, it doesn’t iterate through the array. What code do we need to add to our IBAction that will enable the button to iterate through the array upon a button press? We will work on this part in Week 7.
+     */
 //I believe you will need to run the for loop 
 
 class firstViewController: UIViewController {
 
 // PART 3: Customize ViewController (top) in Storyboard and Create IBActions (3 points)
+    /*
+     1.Customize your VC in Storyboard with 2 labels and 1 button
+     2. Create 2 IBOutlets: 1 per Label and Create 1 IBAction and name it: “buttonPressed”.
+     3. Name your IBOutlets respectively:
+     For the top label please name the outlet: “topLabel”
+     For the bottom label please name the outlet: “bottomLabel”
+     */
+    
     @IBOutlet weak var topLabel: UILabel!
     
     @IBOutlet weak var bottomLabel: UILabel!
@@ -59,7 +67,10 @@ class firstViewController: UIViewController {
         //1. When the button is pressed, set the “bottomLabel” text to display the first item in the array.
     @IBAction func buttonPressed(_ sender: Any)
     {
-        self.bottomLabel.text = favoriteFoodsArray[0]
+        self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+        //OR
+        //self.bottomLabel.text = favoriteFoodsArray[0]
+
 
     }
     
---
+++ HackwichFive.9.27.23/secondViewController.swift
@@ -0,0 +1,50 @@
+//
+//  secondViewController.swift
+//  HackwichFive.9.27.23
+//
+//  Created by Aina Kodaira on 9/29/23.
+//
+
+import UIKit
+/*
+ PROBLEM SET for TAB 2: My Favorite Shows/Movies (10 points total)
+
+For Tab 2, you will follow steps part 2-7. (instructions written in "firstViewController" file
+You will create an array that contains your 5 favorite shows or movies and display the third item in your array.
+Record your simulator and submit the link to your recording via Laulima for this assignment. (5 points)
+*/
+
+class secondViewController: UIViewController {
+
+//PART 5
+// ??? Unsure if we still need to repeat part 5-#1: declare & assign currentIndex to 0
+    var currentIndex = 0
+    var favoriteShowsMoviesArray = ["Kiki's Delivery Service", "Courage the Cowardly Dog", "Deadpool", "Unwrapped", "The British Baking Show"]
+//PART 3
+    @IBOutlet weak var topLabelVC2: UILabel!
+    
+    @IBOutlet weak var bottomLabelVC2: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    
+    @IBAction func buttonPressedVC2(_ sender: Any)
+    {
+        self.bottomLabelVC2.text = favoriteShowsMoviesArray[2]
+    }
+}
