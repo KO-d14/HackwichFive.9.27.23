78755f8193c0d838512a01a725a3c73c2a7f2da4
KO-d14 <ainajk@hawaii.edu>
2023-10-05 11:32:02 -1000

    -Added lines of comments regarding the instructions for the assignment - COMPLETED PARTS 1-8; Ran simulator and worked fine

--- HackwichFive.9.27.23/firstViewController.swift
+++ HackwichFive.9.27.23/firstViewController.swift
@@ -44,6 +44,10 @@ class firstViewController: UIViewController {
     
     @IBOutlet weak var bottomLabel: UILabel!
     
+//For WEEK 7 Part 1 #3.a
+    //  a. → First: Create an IBOutlet for the Button and name it “buttonLabel”. This is so we can access and set the title for our button.
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -70,9 +74,87 @@ class firstViewController: UIViewController {
         self.bottomLabel.text = favoriteFoodsArray[currentIndex]
         //OR
         //self.bottomLabel.text = favoriteFoodsArray[0]
+        
+
+// ************ Week 7: Hackwich Five Part 2: Iterating Through Arrays ******************
+// In Breakout Rooms:
+// Part 1: Set up a if/else statement to iterate through each item in array upon button press (10 points)
+    /* 1. In order to iterate through the array, you will need to:
+            Create a conditional statement that compares currentIndex to the number of items in the array. HINT: How do you get the number of items in the array? Refer to Project #1 Guidelines
+         
+                2. (written in above designated line of code)
+          
+                3. Programmatically in code, set the Button’s title to “Next” to indicate that there are still items in the array to display.
+     
+                4. (written in above designated line of code)
+                5. (written in above designated line of code)
+
+         Stop: Commit & Push. Suggested Commit Message: Completed Parts 1-8.
+         Proceed to the Problem Sets */
+
+       
+       
+        if currentIndex < favoriteFoodsArray.count - 1
+            {
+//FOR WEEK 7 Part 1 #2. Set the bottomLabel to display the item in the array based on the conditional statement you set up.
+                self.bottomLabel.text = favoriteFoodsArray[currentIndex]
+//For WEEK 7 Part 1 #3.b
+            /* → Second: Set the state of the button (which is a UIcontrol) to a normal state or an enabled state. Type the line of code in blue below into the if code block. https://developer.apple.com/documentation/uikit/uicontrol/state
+                        buttonLabel.setTitle("Next", for: UIControl.State.normal) */
+                buttonLabel.setTitle("Next", for: UIControl.State.normal)
+//For WEEK 7 Part 1 #4
+            /* xIn order to access the next element in the array, we need to increase currentIndex by 1. Add the line of code in blue below into the if code block.
+                                        currentIndex += 1
+             */
+                currentIndex += 1
+            }//end bracket of if statment
+            else
+            {
+//For WEEK 7 Part 1 #5
+                /* Now, we need to disable the button once all items in the array have been displayed in the label. Type the line of code in blue below into the else code block.
+                 
+                                    print(“button has been disabled”)
+                                (buttonLabel as! UIButton).isEnabled=false
+                 */
+                print("button has been disabled")
+            //THIS CODE CAME OUT AS ERROR --> (buttonLabel as! UIButton).isEnabled = false
+                (buttonLabel!).isEnabled = false
+
+            }//end for else statement
+        
+
+    }//end for IBAction buttonPressed
+    
+// ------------ COMMITED AND PUSHED- wrote "Completed Parts 1-8" -----------------
+    
+//IGNORE CODE BELOW - MAY DELETE LATER
+    /* for (currentIndex; currentIndex < favoriteFoodsArray.length; currentIndex++)
+         //food in favoriteFoodsArray.count {
+         if currentIndex < favoriteFoodsArray.length
+     {
+         
+     }
+     else
+     {
+         
+     } */
+
+// WEEK 7: ---------- Problem Sets 1 & 2 & Simulator Recording: ----------------
+//Problem Set #1: Adding Comments to Code in IBAction in First Tab (6 points)
+        //1. Write a comment in your own words for each line of code in the IBAction code block. You should have 6 comments total. Please begin each comment with comment #1, comment #2, etc.
 
 
-    }
-    
-    
+ //Problem Set #2: Please repeat Part 1 for your Tab #2 (6 points)
+
+ //Simulator Recording (6 points):
+ Record your simulator showing your final build for tabs 1 & 2 and upload your link to Laulima for this assignment. Please be sure that you have adjusted the size and display lines for your labels so that the text doesnʻt get cut off. If your text is cut off, you will incur -2 point deduction from your recording grade.
+
+
+ //Stop: Commit & Push
+
+
+
+
+
 }
+
